1fd3ff26d17ceec4ce0bb7c60294d918b49533b4
0812Sean <dachenxuan@gmail.com>
2024-08-28 16:23:08 -0500

    “Done”

---
+++ main_app/admin.py
@@ -0,0 +1,9 @@
+
+# Register your models here.
+from django.contrib import admin
+from .models import Cat, Feeding, Toy
+
+admin.site.register(Cat)
+admin.site.register(Feeding)
+admin.site.register(Toy)
+
